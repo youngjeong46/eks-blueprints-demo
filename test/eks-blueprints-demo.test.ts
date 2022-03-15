cbbb4fd4bd422270a804329ff4635d7bda2ef453
Young Jeong <youngjeong46@gmail.com>
2022-03-15 15:10:30 -0500

    Initial commit

---
+++ test/eks-blueprints-demo.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as EksBlueprintsDemo from '../lib/eks-blueprints-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eks-blueprints-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EksBlueprintsDemo.EksBlueprintsDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
