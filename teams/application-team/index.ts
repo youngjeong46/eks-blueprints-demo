896e5948a29b6ffd073b52fe087c0f6c116dd010
Young Jeong <youngjeong46@gmail.com>
2022-03-15 18:02:24 -0500

    adding teams

--- teams/application-team/index.ts
+++ teams/application-team/index.ts
@@ -1,10 +1,11 @@
+import { ArnPrincipal } from '@aws-cdk/aws-iam';
 import { ApplicationTeam } from '@aws-quickstart/ssp-amazon-eks';
-
 
 export class TeamApplication extends ApplicationTeam {
     constructor(name: string) {
         super({
             name: name,
+            users: [new ArnPrincipal(`arn:aws:iam:590259161827:user/platform`)],
             userRoleArn: "arn:aws:iam::590259161827:role/application-team-role"
         });
     }
--- teams/platform-team/index.ts
+++ teams/platform-team/index.ts
@@ -1,9 +1,11 @@
+import { ArnPrincipal } from '@aws-cdk/aws-iam';
 import { PlatformTeam } from '@aws-quickstart/ssp-amazon-eks';
 
 export class TeamPlatform extends PlatformTeam {
     constructor() {
         super({
             name: "platform",
+            users: [new ArnPrincipal(`arn:aws:iam:590259161827:user/platform`)],
             userRoleArn: "arn:aws:iam::590259161827:role/platform-team-role",
         })
     }
