c64d34e78d3353988adb8a75788219a2ccf5a8b5
Young Jeong <youngjeong46@gmail.com>
2022-03-17 13:22:18 -0500

    added ContainerInsightsAddOn

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -1,18 +1,18 @@
 import * as cdk from '@aws-cdk/core';
-import * as ssp from '@aws-quickstart/ssp-amazon-eks';
+import * as blueprints from '@aws-quickstart/ssp-amazon-eks';
 import { TeamPlatform, TeamApplication } from '../teams';
 
 export default class PipelineConstruct extends cdk.Construct{
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps){
     super(scope,id)
 
-    const blueprint = ssp.EksBlueprint.builder()
+    const blueprint = blueprints.EksBlueprint.builder()
     .account(props?.env?.account)
     .region(props?.env?.region)
-    .addOns()
+    .addOns(new blueprints.ContainerInsightsAddOn())
     .teams(new TeamPlatform(), new TeamApplication('burnham'));
   
-    ssp.CodePipelineStack.builder()
+    blueprints.CodePipelineStack.builder()
       .name("blueprints-demo-pipeline")
       .owner("youngjeong46")
       .repository({
