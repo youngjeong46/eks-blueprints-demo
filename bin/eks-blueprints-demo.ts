e937633e7ecc0d14ca1ea4e1a5f81ec077555284
Young Jeong <youngjeong46@gmail.com>
2022-03-17 19:32:42 -0500

    customized provider

--- bin/eks-blueprints-demo.ts
+++ bin/eks-blueprints-demo.ts
@@ -1,5 +1,4 @@
 import * as cdk from '@aws-cdk/core';
-import ClusterConstruct from '../lib/eks-blueprints-demo-stack';
 import PipelineConstruct from '../lib/pipeline'; // IMPORT OUR PIPELINE CONSTRUCT
 
 
@@ -7,7 +6,5 @@ const app = new cdk.App();
 const account = process.env.CDK_DEFAULT_ACCOUNT!;
 const region = process.env.CDK_DEFAULT_REGION;
 const env = { account, region }
-//Define Custom AMI, Instance Type, etc. - Generic Cluster Provider
 
-new ClusterConstruct(app, 'cluster', { env });
 new PipelineConstruct(app, 'pipeline', { env });
--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -1,4 +1,6 @@
 import * as cdk from '@aws-cdk/core';
+import * as eks from '@aws-cdk/aws-eks';
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as blueprints from '@aws-quickstart/ssp-amazon-eks';
 import { TeamPlatform, TeamApplication } from '../teams';
 
@@ -6,11 +8,35 @@ export default class PipelineConstruct extends cdk.Construct{
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps){
     super(scope,id)
 
-    // use Custom AMI, Instance Type, etc.
+    // Customized Cluster Provider
+    const clusterProvider = new blueprints.GenericClusterProvider({
+      version: eks.KubernetesVersion.V1_21,
+      managedNodeGroups: [
+        {
+            id: "mng1",
+            amiType: eks.NodegroupAmiType.AL2_X86_64,
+            instanceTypes: [new ec2.InstanceType('m5.2xlarge')]
+        },
+        {
+            id: "mng2-custom",
+            instanceTypes: [new ec2.InstanceType('m5.2xlarge')],
+            nodeGroupCapacityType: eks.CapacityType.SPOT,
+            customAmi: {
+              machineImage: ec2.MachineImage.genericLinux({
+                  'us-east-1': 'ami-0e1b6f116a3733fef',
+                  'us-west-1': 'ami-0167be2eb86ccfbb6',
+                  'us-east-2': 'ami-0656dd273bd6e9a2f'
+              }),
+            }
+        }
+      ]
+    });
+
     const blueprint = blueprints.EksBlueprint.builder()
     .account(props?.env?.account)
+    .clusterProvider(clusterProvider)
     .region(props?.env?.region)
-    .addOns(new blueprints.ContainerInsightsAddOn())
+    .addOns()
     .teams(new TeamPlatform(), new TeamApplication('burnham'));
   
     blueprints.CodePipelineStack.builder()
